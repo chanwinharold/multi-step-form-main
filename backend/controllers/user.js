fae68303f1053132f6f4c90a265f4c80438df936
chanwinharold <chanwinharold@gmail.com>
2025-12-11 15:10:29 +0100

    Update UML Models and Tables

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,19 +1,15 @@
 const db = require("../models/DB");
 
 exports.createUser = (req, res, next) => {
-    delete req.body.id;
-    if (req.body) {
-        const query = `
-            INSERT INTO Users(name, email, tel, id_plan)
-            VALUES (?, ?, ?, NULL)
+  delete req.body.id;
+  const query = `
+            INSERT INTO Users(name, email, tel, id_plan, id_period)
+            VALUES (?, ?, ?, NULL, NULL)
         `;
-        db.run(query, [req.body.name, req.body.email, req.body.tel], 
-            (error) => {
-                if (error) {
-                    res.status(400).json({error});
-                }
-                res.status(201).json({message: "User informations saved successfully !"})
-            }
-        )
+  db.run(query, [req.body.name, req.body.email, req.body.tel], (error) => {
+    if (error) {
+      res.status(400).json({ error });
     }
-}
+    res.status(201).json({ message: "User informations saved successfully !" });
+  });
+};
--- backend/models/queries.sql
+++ backend/models/queries.sql
@@ -1,54 +1,55 @@
 -- Active: 1765363679242@@127.0.0.1@3306
 
-DROP TABLE IF EXISTS AddonPrices;
-CREATE TABLE AddonPrices (
-    id_addon_price INTEGER PRIMARY KEY AUTOINCREMENT,
-    price INTEGER NOT NULL
-);
-
-DROP TABLE IF EXISTS PlanPrices;
-CREATE TABLE PlanPrices (
-    id_plan_price INTEGER PRIMARY KEY AUTOINCREMENT,
-    price INTEGER NOT NULL
-);
-
 DROP TABLE IF EXISTS Periods;
 CREATE TABLE Periods (
     id_period INTEGER PRIMARY KEY AUTOINCREMENT,
-    period VARCHAR(20) NOT NULL CHECK (period IN ("Monthly", "Yearly")),
-    id_addon_price INTEGER REFERENCES AddonPrices(id_addon_price),
-    id_plan_price INTEGER REFERENCES PlanPrices(id_plan_price)
+    name VARCHAR(40) UNIQUE CHECK (name IN ('Monthly', 'Yearly'))
 );
 
 DROP TABLE IF EXISTS Plans;
 CREATE TABLE Plans (
     id_plan INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(20) NOT NULL,
-    id_period INTEGER REFERENCES Periods(id_period)
+    title VARCHAR(40) NOT NULL UNIQUE,
+    imgUrl VARCHAR(255) NOT NULL UNIQUE,
+    price_monthly DECIMAL NOT NULL,
+    price_yearly DECIMAL NOT NULL
 );
 
 DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
     id_user INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(20) UNIQUE NOT NULL,
-    email VARCHAR(50) UNIQUE NOT NULL,
-    tel VARCHAR(20) UNIQUE NOT NULL,
-    id_plan INTEGER REFERENCES Plans(id_plan)
+    name VARCHAR(40) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    tel VARCHAR(40) UNIQUE NOT NULL,
+    id_plan INTEGER REFERENCES Plans(id_plan),
+    id_period INTEGER REFERENCES Periods(id_period)
 );
 
 DROP TABLE IF EXISTS Addons;
 CREATE TABLE Addons (
     id_addon INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(20) UNIQUE NOT NULL,
+    title VARCHAR(40) UNIQUE NOT NULL,
     description VARCHAR(255) UNIQUE NOT NULL,
-    tel VARCHAR(20) UNIQUE NOT NULL,
-    id_plan INTEGER REFERENCES Plans(id_plan)
+    price_monthly DECIMAL NOT NULL,
+    price_yearly DECIMAL NOT NULL
 );
 
-DROP TABLE IF EXISTS Choices;
-CREATE TABLE Choices(
+DROP TABLE IF EXISTS ChoicesAddons;
+CREATE TABLE ChoicesAddons(
     id_user INTEGER REFERENCES Users(id_user),
     id_addon INTEGER REFERENCES Addons(id_addon)
 );
 
-/* INSERT INTO PlanPrices() */
+INSERT INTO Periods(name)
+VALUES  ("Monthly"),
+        ("Yearly");
+
+INSERT INTO Plans(title, imgUrl, price_monthly, price_yearly)
+VALUES  ('Arcade', 'icon-arcade.svg', 9, 90),
+        ('Advanced', 'icon-advanced.svg', 12, 120),
+        ('Pro', 'icon-pro.svg', 15, 150);
+
+INSERT INTO Addons(title, description, price_monthly, price_yearly)
+VALUES  ('Online service', 'Access to multiplayer games', 1, 10),
+        ('Larger storage', 'Extra 1TB of cloud save', 2, 20),
+        ('Customizable profile', 'Custom theme on your profile', 2, 20);
