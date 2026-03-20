20ca603d45e93f0dbb20673a78b6515215d2ecda
chanwinharold <chanwinharold@gmail.com>
2026-03-20 04:21:33 +0100

    axios api url updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import axios from "axios"
 
-axios.defaults.baseURL = "https://multi-step-form-main-b5p1.vercel.app"
+axios.defaults.baseURL = "https://multi-step-form-main-iav4.onrender.com"
 axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')).render(
